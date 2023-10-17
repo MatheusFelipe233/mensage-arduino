fba8af200ee9baaf80b6ce6fe83bf537916f635e
Lucas <anjoslucas7@gmail.com>
2023-10-17 14:58:04 -0400

    alterei os serius

--- projetos/testdecomunicacao/test/serius.py
+++ projetos/testdecomunicacao/test/serius.py
@@ -1,5 +1,6 @@
 import os
 import serial
+import subprocess
 
  # Abre a porta serial
 ser = serial.Serial('COM4', 9600)  # Substitua 'COMx' pelo nome da porta do Arduino
@@ -8,8 +9,8 @@ while True:
     data = ser.readline().decode().strip()
     if data == "alerta":
         # Navegue até o diretório onde o programa está localizado
-        programa_dir = "/Users/anjos/projetos/mensage-arduino/projetos/testdecomunicacao/test/serius.py"
-        os.chdir(programa_dir)
+        programa_dir = r'C:\Users\anjos\projetos\mensage-arduino\projetos\testdecomunicacao\test\comunic.py'
+        os.system('python ' + programa_dir)
         
         # Execute o programa
         os.system("python comunic.py")
