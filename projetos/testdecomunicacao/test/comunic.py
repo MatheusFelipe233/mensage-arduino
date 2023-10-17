529506729af920acdb82f4f4ae8199b56d93056d
MatheusFelipe233 <148176189+MatheusFelipe233@users.noreply.github.com>
2023-10-16 23:35:22 -0400

    Add files via upload

---
+++ projetos/testdecomunicacao/test/comunic.py
@@ -0,0 +1,13 @@
+from twilio.rest import Client
+
+account_sid = 'AC190a9418d8853a70ae2707247e73aa9b'
+auth_token = '06040c26b3a16cb3e0167d66e0163bf0'
+client = Client(account_sid, auth_token)
+
+message = client.messages.create(
+  from_='whatsapp:+14155238886',
+  body='ALERTA:  Risco de ataque!!!!!',
+  to='whatsapp:+559295143900'
+)
+
+print(message.sid)
