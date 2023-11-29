01f1bd887ffa128dee8225b42fd5000f32fa3c8a
Lucas <anjoslucas7@gmail.com>
2023-11-28 22:04:21 -0400

    commit

--- program.py
+++ program.py
@@ -4,7 +4,7 @@ import time
 import serial
 
 # constante para limitar a quantidade de requisições a serem feitas
-NUMERO_REQUISICOES = 3
+# NUMERO_REQUISICOES = 3
 
 # link do banco de dados realtime com firebase
 link = "https://cardio-sensor-default-rtdb.firebaseio.com/"
@@ -12,7 +12,7 @@ link = "https://cardio-sensor-default-rtdb.firebaseio.com/"
 # abrir a porta serial para comunicação com arduino
 ser = serial.Serial('COM4', 9600)  
 
-cont = 0
+# cont = 0
 # while cont < NUMERO_REQUISICOES :
 while True:
     frequencia = ser.readline().decode().strip() #frequencia lida no serial do arduino
@@ -24,7 +24,7 @@ while True:
         # Criar uma estrutura do tipo cardio
         dados = {
             'frequencia' : f'{frequencia}',
-            'timestamp' : timestamp
+            'timestamp' : timestamp,
         }
         # faz uma requisição post para o firebase com o link e passando o dado do tipo cardio para o firebase persistir em núvem (guardar)
         requisicao = requests.post(f'{link}/cardio/.json', data=json.dumps(dados))
@@ -32,7 +32,7 @@ while True:
         print(f'REQUISICAO\n {requisicao}')
 
     time.sleep(2)
-    cont += 1  
+    # cont += 1  
 
 """ 
     referencias:
