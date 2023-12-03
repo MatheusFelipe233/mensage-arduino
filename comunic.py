683108a5af58d26a639f2cc1b54b47c701d7a168
Lucas <anjoslucas7@gmail.com>
2023-12-03 12:50:06 -0400

    add(comunic): arquivo de exemplo     - criação de arquivo de exemplo de comunicação com whatsapp via twilio

--- comunic.py
+++ comunic.py
@@ -1,13 +1,17 @@
 from twilio.rest import Client
 
-account_sid = 'AC190a9418d8853a70ae2707247e73aa9b'
-auth_token = '06040c26b3a16cb3e0167d66e0163bf0'
+account_sid = 'AC499cf50728ad909a813c46d2874777f5'
+auth_token = '573ad38ff8032c8c66e913b567d82f84'
 client = Client(account_sid, auth_token)
 
 message = client.messages.create(
   from_='whatsapp:+14155238886',
-  body='Cuidado, melhor diminuir o esforço!!!!',
-  to='whatsapp:+559295143900'
+  body='preciso passar em seph porra >_<',
+  to='whatsapp:+559282605819'
 )
 
 print(message.sid)
+
+#comentário
+
+#segundo comentário
